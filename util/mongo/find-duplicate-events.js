35b170ec440f71fd38e935c1b3b5b882702a082b
Liam Bowen <LiamBowen@gmail.com>
2015-05-18 01:49:14 +0000

    Script to find duplicates works.

---
+++ util/mongo/find-duplicate-events.js
@@ -0,0 +1,43 @@
+// This shouldn't ever need to be run unless the existing data needs re-importing
+// Takes 40 minutes to clear 10 records :(
+db.contributions.aggregate(
+    [
+        {
+            "$match": {
+                "_event_id": { "$exists" : true },
+            },
+        },
+        {
+            "$group": {
+                "_id": { "_event_id": "$_event_id" },
+                "uniqueIds": { "$push": "$_id" },
+                "count": { "$sum": 1 },
+            },
+        },
+        {
+            "$match": {
+                "count": { "$gt": 1 },
+            }
+        },
+        {
+            $out : "duplicates",
+        }
+    ],
+    {
+        "allowDiskUse": true,
+    }
+);
+// .forEach(function(doc) {
+//     doc.uniqueIds.shift();
+//     printjson(doc);
+//     var wRes = db.contributions.remove(
+//         {
+//             "_id": {
+//                 "$in": doc.uniqueIds,
+//             },
+//         },
+//         {
+//             "justOne": true,
+//         });
+//     print(wRes);
+// });
