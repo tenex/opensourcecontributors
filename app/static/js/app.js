82b831925dd5445f2209e679b082f9305d7dffe9
Liam Bowen <LiamBowen@gmail.com>
2015-05-19 02:43:42 +0000

    Updated backend to send expected data.
    Also cosmetic fixes to frontend.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,26 +1,115 @@
 (function() {
     var app = angular.module('ghca', ['angularMoment']);
 
-    app.controller("UserController", ["$http","$log", function($http, $log) {
-        this.user = "";
+    app.constant("moment", moment);
+
+    app.controller("UserController", ["$http","$log", "moment", function($http, $log, moment) {
+        // TODO Is this how you fake enums in JS?! I hate JS.
+        this.tabs = {
+            none: 0,
+            repoList: 1,
+            eventList: 2
+        };
+        this.range = function(x) {
+            return Array.apply(null, Array(x)).map(function (_, i) {return i+1;});
+        };
+
+
+        this.currentTab = this.tabs.repoList;
+        this.isCurrentTab = function(t) {
+            return this.currentTab === t;
+        };
+        this.setCurrentTab = function(t) {
+            this.currentTab = t;
+            if(t == this.tabs.eventList) {
+                this.getGHEvents();
+            }
+        };
+
+        this.username = "";
+        this.processedUsername = ""; // The data below is for...
         this.userUrl = "";
         this.eventCount = 0;
         this.repos = [];
-        // Have we retrieved the user's information (except all events)?
+
+        // Event-related stuff
+        this.eventPages = {}; // a cache of sorts
+        this.eventPageCount = 0;
+        this.currentEventPage = 1;
+        this.events = []; // the current page
+        this.eventPageSize = 50; // constant
+        this.eventCount = 0;
+
+        // Have we retrieved the user's information (except a list of their events)?
         this.processed = false;
+        this.processing = false;
+        this.hasResults = false;
+
+        this.setCurrentEventsPage = function(i) {
+            this.currentEventPage = i;
+            this.getGHEvents();
+        };
+
+        this.nextEventsPage = function() {
+            if (this.currentEventPage !== this.eventPageCount) {
+                this.setCurrentEventsPage(this.currentEventPage+1);
+            }
+        };
+
+        this.previousEventsPage = function() {
+            if (this.currentEventPage !== 1) {
+                this.setCurrentEventsPage(this.currentEventPage-1);
+            }
+        };
+
+        this.getGHEvents = function() {
+            // Cache :)
+            if (this.eventPages[this.currentEventPage]) {
+                this.events = this.eventPages[this.currentEventPage];
+                return;
+            }
+
+            var userCtrl = this;
+            $http.get('/user/'+this.processedUsername+'/events/'+this.currentEventPage, {})
+                .success(function(data) {
+                    userCtrl.eventPages[userCtrl.currentEventPage] = data.events;
+                    userCtrl.events = data.events;
+                })
+                .error(function(data) {
+                    $log.error(data);
+                });
+        };
 
         this.setUser = function() {
-            var user = this;
-            $http.get('/user/'+this.user, {}).success(function(data) {
-                user = data;
-                user.userUrl = "https://github.com/"+data.user;
-                user.processed = true;
-            });
+            var userCtrl = this;
+            this.processed = false;
+            this.processing = true;
+            $http.get('/user/'+this.username, {})
+                .success(function(data) {
+                    userCtrl.processing = false;
+                    userCtrl.eventCount = data.eventCount;
+                    userCtrl.hasResults = data.eventCount ? true : false;
+                    userCtrl.eventPageCount = Math.ceil(
+                        data.eventCount / userCtrl.eventPageSize);
+                    userCtrl.multipleEventPages = (
+                        userCtrl.eventCount > userCtrl.eventPageSize);
+                    userCtrl.repos = data.repos;
+                    userCtrl.userUrl = "https://github.com/"+data.username;
+                    userCtrl.processedUsername = data.username;
+                    userCtrl.processed = true;
+                    userCtrl.processing = false;
+                })
+                .error(function(data) {
+                    $log.error(data);
+                    userCtrl.processing = false;
+                });
+
         };
     }]);
 
     app.controller("EventController", ["$http","$log", function($http,$log) {
-        this.event = {};
+
+
     }]);
 
 })();
--- app/views.py
+++ app/views.py
@@ -11,44 +11,42 @@ PAGE_SIZE = 50
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/summary/<user>')
-def summary(user):
+@app.route('/user/<username>')
+def user(username):
     collection = mongo.db.contributions
     criteria = {
-        '_user_lower': query.lower(),
+        '_user_lower': username.lower(),
     }
     repos = collection.find(criteria)
-    repos = repos.distinct('repo').sort("repo", ASCENDING)
+    repos = repos.distinct('repo')
+    repos.sort()
 
-    total = collection.find(criteria).count()
+    event_count = collection.find(criteria).count()
 
     summary = {
+        "username": username,
+        "eventCount": event_count,
         "repos": repos,
     }
     return jsonify(**summary)
 
-@app.route('/events/<user>')
-@app.route('/events/<user>/<int:page>')
-def events(user, page=1):
+@app.route('/user/<username>/events')
+@app.route('/user/<username>/events/<int:page>')
+def events(username, page=1):
     collection = mongo.db.contributions
     criteria = {
-        '_user_lower': query.lower(),
+        '_user_lower': username.lower(),
     }
 
     skip = (page-1) * PAGE_SIZE
-    total_pages = math.ceil(float(total) / PAGE_SIZE)
-    repos = collection.find(criteria).distinct('repo')
+    #total_pages = math.ceil(float(total) / PAGE_SIZE)
 
     events = collection.find(criteria)
     events = events.sort("created_at", DESCENDING)
     events = events.skip(skip).limit(PAGE_SIZE)
     events = list(events)
     events = {
-        "repos": repos,
         "events": events,
-        "user": query,
-        "total": total,
-        "pageCount": total_pages,
         "start": skip+1,
         "end": skip+len(events),
         "currentPage": page,
