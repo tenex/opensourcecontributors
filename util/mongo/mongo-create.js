e95e138566b44ea0694f3f343913a3cb51b43d07
Liam Bowen <LiamBowen@gmail.com>
2015-06-06 20:55:15 -0400

    the create script is really inaccurate

--- util/mongo/mongo-create.js
+++
@@ -1,6 +0,0 @@
-use contributions
-db.contributions.createIndex( { "_org_lower": 1 });
-db.contributions.createIndex( { "_repo_lower": 1 });
-db.contributions.createIndex( { "_user_lower": 1 });
-db.contributions.createIndex( { "created_at": 1 });
-db.contributions.createIndex( { "_event_id": 1 }, { unique: true }); // TODO use sparse; not unique with nulls
