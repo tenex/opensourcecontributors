ae18b5b9cbef0d7e748b4dc0a05fc3435cff9cd0
Liam Bowen <liambowen@gmail.com>
2016-01-04 18:39:31 -0500

    Fix deprecations and reformat services.js

--- app/static/js/services.js
+++ app/static/js/services.js
@@ -1,15 +1,12 @@
-angular.module('ghcaServices', ['ngResource'])
-    .factory('User',
-             ['$resource',
-              function($resource) {
-                  return $resource('/user/:username', {}, {});
-              }])
-    .factory('Event',
-             ['$resource',
-              function($resource) {
-                  return $resource('/user/:username/events/:page', {}, {});
-              }])
-    .factory('Statistics',
-             ['$resource', function($resource) {
-                 return $resource('/stats');
-             }]);
+(function() {
+  var app = angular.module('ghcaServices', ['ngResource']);
+  app.factory('User', ['$resource', function($resource) {
+    return $resource('/user/:username');
+  }]);
+  app.factory('Event', ['$resource', function($resource) {
+    return $resource('/user/:username/events/:page');
+  }]);
+  app.factory('Statistics', ['$resource', function($resource) {
+    return $resource('/stats');
+  }]);
+})();
