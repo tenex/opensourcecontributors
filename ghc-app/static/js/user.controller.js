70c7bace0dcd5777870ff0f807649ab3044c3e87
Liam Bowen <LiamBowen@gmail.com>
2016-02-03 22:20:09 -0500

    Handle Event stuff in here

--- ghc-app/static/js/user.controller.js
+++ ghc-app/static/js/user.controller.js
@@ -5,20 +5,41 @@
                     UserController);
 
     UserController.$inject = [
-        "$log", "User", "username"
+        "$log", "User", "Event", "username"
     ];
 
-    function UserController($log, User, username) {
+    function UserController($log, User, Event, username) {
         var vm = this;
 
+        // User stuff
         vm.username = username;
         vm.userUrl = "";
-
         vm.user = User.get({username: username}, function(user) {
             vm.userUrl = "https://github.com/" + user.username;
         });
 
+        // Event stuff
+        vm.loadingEvents = false;
+        vm.eventPages = {};
+        vm.eventData = {};
+        vm.currentEventPage = 1;
+        vm.eventPageSize = 50;
+        vm.eventPageChanged = function() {
+            fetchEventPage(vm.currentEventPage);
+        };
 
-        $log.debug("UserController: ", username);
+        // Preload
+        fetchEventPage(1);
+
+        function fetchEventPage(pageNumber) {
+            vm.loadingEvents = true;
+            vm.eventData = Event.get({
+                username: username,
+                page: pageNumber
+            }, function(events) {
+                vm.eventPages[pageNumber] = events;
+                vm.loadingEvents = false;
+            });
+        }
     }
 })();
