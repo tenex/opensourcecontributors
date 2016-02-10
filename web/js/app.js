79f9d3b137f3462366569a4c7b509e3e436d9efb
Liam Bowen <LiamBowen@gmail.com>
2016-02-10 18:00:57 -0500

    Not a huge fan of this, but better than b4

--- web/js/app.js
+++ web/js/app.js
@@ -1,10 +1,23 @@
+var angular = require('angular');
+
+require('./services.js');
+require('./truncate.js');
+require('./templates.js');
+
 (function() {
-    angular.module('ghca', [
-        'ghca.services',
-        'angularMoment',
-        'truncate',
-        'ui.bootstrap',
-        'ui.router',
-        'templates'
-    ]);
+  angular.module('ghca', [
+    'ghca.services',
+    'truncate',
+    'templates',
+    require('angular-moment'),
+    require('angular-ui-bootstrap'),
+    require('angular-ui-router')
+  ]);
 })();
+
+require('./routes.js');
+require('./error-handler.js');
+require('./bsod.controller.js');
+require('./search.controller.js');
+require('./user.controller.js');
+require('./stats.controller.js');
