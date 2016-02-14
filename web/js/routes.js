43b08056cc93b73c091348a804a4d3dd91c5bc22
Liam Bowen <LiamBowen@gmail.com>
2016-02-13 21:48:29 -0500

    Add root route + named views

--- web/js/routes.js
+++ web/js/routes.js
@@ -1,36 +1,46 @@
 (function() {
-    angular
-        .module('ghca')
-        .config(ConfigureRoutes);
+  angular
+    .module('ghca')
+    .config(ConfigureRoutes);
 
-    ConfigureRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
+  ConfigureRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-    function ConfigureRoutes($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
-        $stateProvider
-            .state('home', {
-                url: '/',
-                template: '<div class="well"><h3>Search for a user above to begin</h3></div>'
-            })
-            .state('user', {
-                url: '/user/{username}',
-                abstract: true,
-                controller: 'UserController',
-                controllerAs: 'userVm',
-                templateUrl: 'user.common.html',
-                resolve: {
-                    "username": function($stateParams) {
-                        return $stateParams.username;
-                    }
-                }
-            })
-            .state('user.repositories', {
-                url: '/',
-                templateUrl: 'user.repositories.html'
-            })
-            .state('user.events', {
-                url: '/events/{page:int}',
-                templateUrl: 'user.events.html'
-            });
-    }
+  function ConfigureRoutes($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('root', {
+        url: '/',
+        views: {
+          search: {
+            templateUrl: 'search-form.html',
+            controller: 'SearchController',
+            controllerAs: 'search'
+          }
+        }
+      })
+      .state('root.user', {
+        url: '/user/{username}',
+        abstract: true,
+        views: {
+          "@": {
+            controller: 'UserController',
+            controllerAs: 'userVm',
+            templateUrl: 'user.common.html',
+            resolve: {
+              "username": function($stateParams) {
+                return $stateParams.username;
+              }
+            }
+          }
+        }
+      })
+      .state('root.user.repositories', {
+        url: '/',
+        templateUrl: 'user.repositories.html'
+      })
+      .state('root.user.events', {
+        url: '/events/{page:int}',
+        templateUrl: 'user.events.html'
+      });
+  }
 })();
