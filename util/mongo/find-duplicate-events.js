d6598074976bbc4656e375520bd439c345fffafe
Liam Bowen <LiamBowen@gmail.com>
2016-02-16 14:11:52 -0500

    lint this js

--- util/mongo/find-duplicate-events.js
+++ util/mongo/find-duplicate-events.js
@@ -1,31 +1,31 @@
 // This shouldn't ever need to be run unless the existing data needs re-importing
 // Takes 40 minutes to clear 10 records :(
 db.contributions.aggregate(
-    [
-        {
-            "$match": {
-                "_event_id": { "$exists" : true },
-            },
-        },
-        {
-            "$group": {
-                "_id": { "_event_id": "$_event_id" },
-                "uniqueIds": { "$push": "$_id" },
-                "count": { "$sum": 1 },
-            },
-        },
-        {
-            "$match": {
-                "count": { "$gt": 1 },
-            }
-        },
-        {
-            $out : "duplicates",
-        }
-    ],
+  [
     {
-        "allowDiskUse": true,
+      "$match": {
+        "_event_id": { "$exists" : true }
+      },
+    },
+    {
+      "$group": {
+        "_id": { "_event_id": "$_event_id" },
+        "uniqueIds": { "$push": "$_id" },
+        "count": { "$sum": 1 }
+      },
+    },
+    {
+      "$match": {
+        "count": { "$gt": 1 }
+      }
+    },
+    {
+      $out : "duplicates"
     }
+  ],
+  {
+    "allowDiskUse": true
+  }
 );
 // .forEach(function(doc) {
 //     doc.uniqueIds.shift();
