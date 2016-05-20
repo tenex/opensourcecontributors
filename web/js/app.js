2d0e3c2a2f7ba6da9a93721b87b5c50a5742b906
Liam Bowen <LiamBowen@gmail.com>
2016-05-20 08:21:13 -0400

    require chart-related deps

--- web/js/app.js
+++ web/js/app.js
@@ -1,4 +1,5 @@
 var angular = require('angular');
+var chart = require('angular-chart.js');
 
 require('./services.js');
 require('./truncate.js');
@@ -9,17 +10,21 @@ require('./templates.js');
     'ghca.services',
     'truncate',
     'templates',
+    'chart.js',
     require('angular-moment'),
     require('angular-ui-bootstrap'),
     require('angular-ui-router')
   ]).config(function($locationProvider) {
     $locationProvider.html5Mode(true).hashPrefix('!');
-  });
+  })
+  ;
 })();
+
 
 require('./routes.js');
 require('./error-handler.js');
 require('./bsod.controller.js');
+require('./chart.controller.js');
 require('./search.controller.js');
 require('./user.controller.js');
 require('./stats.controller.js');
